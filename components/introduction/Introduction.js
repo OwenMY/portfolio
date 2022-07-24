8c72a0f87b357ec224ca1907cc5f15415e3d6fc1
Owen Yoshishige <owenyoshishige@gmail.com>
2022-07-23 21:20:45 -0700

    create introduction component

--- components/introduction/Introduction.js
+++ components/introduction/Introduction.js
@@ -5,7 +5,9 @@ const Introduction = () => {
     <div className="flex flex-row mt-20 gap-28 justify-center">
     <div className="flex-col">
       <h1 className="text-6xl max-w-xl max-h-screen font-bold text-start">Software Developer And Veteran</h1>
-      <p className="text-start">Websites that run faster then your refridgerator (Im Just kidding) </p>
+      <p className="text-start max-w-[600px]">A curious software developer with a passion for automation and technology.
+      Making websites that run faster then your refridgerator (Im kidding) </p>
+      <p className="text-yellow-300"></p>
     </div>
     <Image
       layout="fixed"

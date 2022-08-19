df4e628a037b79719cee045141f085f0ef3a479b
Owen Yoshishige <owenyoshishige@gmail.com>
2022-08-18 17:17:39 -0700

    restructure elements in modal component

--- components/modal/Modal.js
+++ components/modal/Modal.js
@@ -5,7 +5,13 @@ import {motion} from 'framer-motion';
 const Modal = ({ isShowing, hide, component, projectData = null }) => isShowing ? ReactDOM.createPortal(
   <>
     <div className="fixed inset-0 z-[1040] w-screen h-screen bg-black opacity-90"/>
-    <div className="fixed inset-0 z-[1050] w-[100%] h-[100%] overflow-y-scroll outline-0" aria-modal aria-hidden tabIndex={-1} role="dialog">
+    <div
+      className="fixed inset-0 z-[1050] w-[100%] h-[100%] overflow-y-scroll outline-0"
+      aria-modal
+      aria-hidden
+      tabIndex={-1}
+      role="dialog"
+    >
       <motion.div
         className="relative z-[100] h-[100%] max-w-[100%] lg:max-w-[60%] p-[.5em] mx-auto"
         transition={{duration: 1}}
@@ -15,8 +21,13 @@ const Modal = ({ isShowing, hide, component, projectData = null }) => isShowing 
         viewport={{once: true}}
       >
         <div className="h-auto w-sreen overflow-hidden">
-          <button type="button" className="absolute right-0 top-[0%] text-[2em] text-bold ml-[.3em] px-[0] py-[1rem] border-none cursor-pointer z-[1060]" data-dismiss="modal" aria-label="Close" onClick={hide}>
-              <span aria-hidden="true">&times;</span>
+          <button
+            type="button"
+            className="absolute right-0 top-[0%] text-[2em] text-bold ml-[.3em] px-[0] py-[1rem] border-none cursor-pointer z-[1060]" data-dismiss="modal"
+            aria-label="Close"
+            onClick={hide}
+          >
+            <span aria-hidden="true">&times;</span>
           </button>
           {getModalComponent(component, projectData)}
         </div>
